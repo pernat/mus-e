44f1cd3d6e916cc0593e5ba8b2dc9c57f4fd844d
pernat <pernat87@gmail.com>
2025-08-24 23:50:37 +0200

    projects page setup

--- app/muse_projects/views.py
+++ app/muse_projects/views.py
@@ -1,2 +1,23 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Project, ProjectSetting
 
-# Create your views here.
+
+def projects(request):
+    # result = any_http_request.delay(metric_name="Atlassian Vulnerabilities")
+
+    template = loader.get_template("muse_projects/projects.html")
+    app_list = ProjectSetting.objects.all()
+    print(app_list)
+    if app_list.count() == 0:
+        context = {
+            "app_list_count": 0,
+            "vulnerabilities": 0,
+        }
+    else:
+        context = {
+            "project_list": app_list,
+            "project_list_count": len(app_list),
+            
+        }
+    return HttpResponse(template.render(context, request))
